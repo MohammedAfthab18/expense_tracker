eacddfcf1992e28a4c336a541a0363eb9db2a1b0
Mohammed Afthab <afthab2282@gmail.com>
2024-04-18 19:21:39 +0530

    new expense tracking project #3

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size(50, 50),
+          child: AppBar(
+            backgroundColor: Colors.purple,
+            title : const Text(
+            "Flutter ExpenseTracker",
+            style: TextStyle(
+              color: Color.fromARGB(255, 202, 131, 214),
+            ),
+          ),
+        ),
+        ),
+        backgroundColor: Colors.white,
+        body: const Center(
+          child: Text(
+            "Expense Tracker",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    ),
+  );
+}
