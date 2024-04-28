5b64df39e557fee206404c8d1f54b9917164074e
Mohammed Afthab <afthab2282@gmail.com>
2024-04-28 14:00:08 +0530

    added charts

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
 
-
 final formatter = DateFormat.yMd();
 
-// to generate  a unique id for each task when i click that button 
-//i use uuid package to do the job for me 
-const  uuid = Uuid();
+// to generate  a unique id for each task when i click that button
+//i use uuid package to do the job for me
+const uuid = Uuid();
 
-enum Category { food , travel ,  leisure , work} // custom category 
+enum Category { food, travel, leisure, work } // custom category
 
 const categoryIcons = {
-  Category.food : Icons.lunch_dining,
-  Category.travel : Icons.flight_takeoff,
-  Category.leisure : Icons.movie,
-  Category.work : Icons.work,
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
 };
 
 class Expense {
@@ -24,8 +23,8 @@ class Expense {
     required this.amount,
     required this.date,
     required this.category,
-    
-  }) : id = uuid.v4(); // uuid.v4() function is used to generate unique string id
+  }) : id = uuid
+            .v4(); // uuid.v4() function is used to generate unique string id
 
   final String id;
   final String title;
@@ -34,7 +33,28 @@ class Expense {
   final Category category;
 
   String get formattedDate {
-  return formatter.format(date);
+    return formatter.format(date);
   }
-  }
+}
 
+class ExpenseBucket {
+  ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses.where((expense) => expense.category == category).toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum = sum + expense.amount; // sum = sum + expense.amount
+    }
+    return sum;
+  }
+}
